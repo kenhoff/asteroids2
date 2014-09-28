e46e6c738625aeeb8a7ab7962f8b8860bca19e7f
Ken Hoff <thekenhoff@gmail.com>
2014-09-28 01:05:32 +0000

    moving around the star field

--- ken_game.js
+++ ken_game.js
@@ -5,42 +5,65 @@ window.onload = function() {
 
 	var game = new Phaser.Game("100", "100", Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
+	var random = new Phaser.RandomDataGenerator()
+
 	var logo;
 
 	function preload () {
 
-		game.load.image('logo', 'phaser.png');
-		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.load.image('ship', 'playerShip1_blue.png');
+		game.load.image("star", "star1.png")
+		game.physics.startSystem(Phaser.Physics.P2JS);
 		cursors = game.input.keyboard.createCursorKeys();
 
 	}
 
 	function create () {
 
-		var worldSize = 500
+		var worldSize = 5000
+		var starCount = 1000
+		// create stars here
+
+		for (var i = 0; i < starCount; i++) {
+			game.add.sprite(random.between(0, worldSize), random.between(0, worldSize), "star")
+		}
 		game.world.setBounds(0, 0, worldSize, worldSize)
-		logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-		game.physics.arcade.enable(logo);
+		ship = game.add.sprite(game.world.centerX, game.world.centerY, 'ship');
+		game.physics.p2.enable(ship);
+		ship.anchor.setTo(0.5, 0.5)
+
+		game.camera.follow(ship)
+
 
 	}
 
 	function update () {
-		var cameraSpeed = 1
-		
+		var shipSpeed = 100
+		var shipForwardSpeed = 100
+		var shipRotateSpeed = 10;
+
+
+		ship.body.setZeroForce()
+		ship.body.angularAcceleration = 0;
+
 		// console.log("main game loop")
 		if (cursors.left.isDown) {
-			game.camera.x -= cameraSpeed
+			ship.body.angularForce -= shipRotateSpeed
 		}
-		if (cursors.right.isDown) {
-			game.camera.x += cameraSpeed
+		else if (cursors.right.isDown) {
+			ship.body.angularForce += shipRotateSpeed
 		}
-
 		if (cursors.up.isDown) {
-			game.camera.y -= cameraSpeed
+			ship.body.thrust(shipSpeed)
+		
+		}
+/*
+		if (cursors.up.isDown) {
+			ship.body.acceleration.y = -shipSpeed
 		}
 		if (cursors.down.isDown) {
-			game.camera.y += cameraSpeed
-		}
+			ship.body.acceleration.y = shipSpeed
+		}*/
 	}
 
 	function render () {
